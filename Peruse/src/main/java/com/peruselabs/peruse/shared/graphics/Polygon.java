d1da0c32cd75c094be1eb06a17444afb5363857c
mattAkap <mattakap@gmail.com>
2018-04-20 14:38:05 -0400

    Eid stuff again again (Github app borked)

--- Peruse/src/main/java/com/peruselabs/peruse/shared/graphics/Polygon.java
+++ Peruse/src/main/java/com/peruselabs/peruse/shared/graphics/Polygon.java
@@ -21,182 +21,187 @@ import com.peruselabs.peruse.shared.graphics.api.MayContain;
  * close the loop to create a polygon (a closed) {@link Path}. e.g. Symmetrical
  * <img src= "http://peruselab.com/images/bonds/templates/cycloHexane.svg" />
  * and asymmetrical <img src="https://goo.gl/mfwHgF" width="120px"/>
- *
- *
+ * 
+ * 
  * @author Velusamy K. Velu (kool.velu@gmail.com)
  *
  * @since Jan 25, 2018
  *
  */
 public class Polygon implements Crossable, MayContain {
-    private List<Point> points;
-    private Point start;
-    private Point end;
+	private List<Point> points;
 
-    public Polygon(@NotNull List<Point> points) throws StdException {
-        super();
-        if (points.size() < 2) {
-            throw new StdException("Not enough points to create a polygon");
-        }
+	public Polygon(@NotNull List<Point> points) throws StdException {
+		super();
+		if (points.size() < 2) {
+			throw new StdException("Not enough points to create a polygon");
+		}
+		this.points = points;
+	}
 
-        this.points = points;
-    }
+	@Override
+	public boolean contains(Point aPoint) {
 
-    @Override
-    public boolean contains(Point aPoint) {
-        int j = points.size() - 1;
-        boolean oddNodes = false;
+		int j = points.size() - 1;
+		boolean oddNodes = false;
 
-        for (int i = 0; i < points.size(); i++)
-        {
-            if (points.get(i).getY() < aPoint.getY() && points.get(j).getY() >= aPoint.getY() ||
-                    points.get(j).getY() < aPoint.getY() && points.get(i).getY() >= aPoint.getY())
-            {
-                if (points.get(i).getX() +
-                        (aPoint.getY() - points.get(i).getY())/(points.get(j).getY() - points.get(j).getY()) * (points.get(j).getX() - points.get(i).getX()) < aPoint.getX())
-                {
-                    oddNodes = !oddNodes;
-                }
-            }
-            j = i;
-        }
+		for (int i = 0; i < points.size(); i++) {
+			if (points.get(i).getY() < aPoint.getY() && points.get(j).getY() >= aPoint.getY()
+					|| points.get(j).getY() < aPoint.getY() && points.get(i).getY() >= aPoint.getY()) {
+				if (points.get(i).getX()
+						+ (aPoint.getY() - points.get(i).getY()) / (points.get(j).getY() - points.get(j).getY())
+								* (points.get(j).getX() - points.get(i).getX()) < aPoint.getX())
 
-        return oddNodes;
+				{
+					oddNodes = !oddNodes;
+				}
+			}
+			j = i;
+		}
 
-    }
+		return oddNodes;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Polygon other = (Polygon) obj;
-        if (points == null) {
-            if (other.points != null)
-                return false;
-        } else if (!points.equals(other.points))
-            return false;
+	}
 
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Polygon other = (Polygon) obj;
+		if (points == null) {
+			if (other.points != null)
+				return false;
+		} else if (!points.equals(other.points))
+			return false;
+		return true;
+	}
 
-    public List<Point> getIntersections(Crossable aLine) {
-        Point firstPoint = new Point();
-        Point secondPoint = new Point();
-        List<Point> listOfIntersections = new ArrayList<Point>();
-        Point intersectedPoint = new Point();
-        for (int i = 0; i < this.points.size()-1; i++) {
-            if (i == 0) {
-                firstPoint = this.points.get(i);
-                secondPoint = this.points.get(this.points.size()-1);
-                Line2D line1 = new Line2D.Double(firstPoint.getX(), firstPoint.getY(), secondPoint.getX(), secondPoint.getY());
-                Line2D line2 = new Line2D.Double(((Line) aLine).getStart().getX(),((Line) aLine).getStart().getY(),((Line) aLine).getEnd().getX(),((Line) aLine).getEnd().getY());
-                boolean result = line2.intersectsLine(line1);
-                if (!result) {
-                }
-                else {
-                    final double x1,y1, x2,y2, x3,y3, x4, y4;
+	public List<Point> getIntersections(Crossable aLine) {
+		Point firstPoint = new Point();
+		Point secondPoint = new Point();
+		List<Point> listOfIntersections = new ArrayList<Point>();
+		Point intersectedPoint = new Point();
+		for (int i = 0; i < this.points.size() - 1; i++) {
+			if (i == 0) {
+				firstPoint = this.points.get(i);
+				secondPoint = this.points.get(this.points.size() - 1);
+				Line2D line1 = new Line2D.Double(firstPoint.getX(), firstPoint.getY(), secondPoint.getX(),
+						secondPoint.getY());
+				Line2D line2 = new Line2D.Double(((Line) aLine).getStart().getX(), ((Line) aLine).getStart().getY(),
+						((Line) aLine).getEnd().getX(), ((Line) aLine).getEnd().getY());
+				boolean result = line2.intersectsLine(line1);
+				if (!result) {
 
-                    x1 = firstPoint.getX();
-                    y1 = firstPoint.getY();
-                    x2 = secondPoint.getX();
-                    y2 = secondPoint.getY();
-                    x3 = ((Line) aLine).getStart().getX();
-                    y3 = ((Line) aLine).getStart().getY();
-                    x4 = ((Line) aLine).getEnd().getX();
-                    y4 = ((Line) aLine).getEnd().getY();
+				} else {
+					final double x1, y1, x2, y2, x3, y3, x4, y4;
 
-                    double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
-                    if (denom == 0.0) { // Lines are parallel.
-                    }
-                    double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3))/denom;
-                    double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3))/denom;
-                    if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
-                        // Get the intersection point.
-                        intersectedPoint = new Point((x1 + ua*(x2 - x1)),(y1 + ua*(y2 - y1)));
-                        listOfIntersections.add(intersectedPoint);
+					x1 = firstPoint.getX();
+					y1 = firstPoint.getY();
+					x2 = secondPoint.getX();
+					y2 = secondPoint.getY();
+					x3 = ((Line) aLine).getStart().getX();
+					y3 = ((Line) aLine).getStart().getY();
+					x4 = ((Line) aLine).getEnd().getX();
+					y4 = ((Line) aLine).getEnd().getY();
 
-                    }
-                }
-            }
+					double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+					if (denom == 0.0) { // Lines are parallel.
 
-            firstPoint = this.points.get(i);
-            secondPoint = this.points.get(i+1);
+					}
+					double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
+					double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+					if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
+						// Get the intersection point.
+						intersectedPoint = new Point((x1 + ua * (x2 - x1)), (y1 + ua * (y2 - y1)));
+						listOfIntersections.add(intersectedPoint);
 
-            Line2D line1 = new Line2D.Double(firstPoint.getX(), firstPoint.getY(), secondPoint.getX(), secondPoint.getY());
-            Line2D line2 = new Line2D.Double(((Line) aLine).getStart().getX(),((Line) aLine).getStart().getY(),((Line) aLine).getEnd().getX(),((Line) aLine).getEnd().getY());
-            boolean result = line2.intersectsLine(line1);
-            if (!result) {
-            }
-            else {
-                final double x1,y1, x2,y2, x3,y3, x4,y4;
+					}
+				}
 
-                x1 = firstPoint.getX();
-                y1 = firstPoint.getY();                
-				x2 = secondPoint.getX();                
+			}
+
+			firstPoint = this.points.get(i);
+			secondPoint = this.points.get(i + 1);
+
+			Line2D line1 = new Line2D.Double(firstPoint.getX(), firstPoint.getY(), secondPoint.getX(),
+					secondPoint.getY());
+			Line2D line2 = new Line2D.Double(((Line) aLine).getStart().getX(), ((Line) aLine).getStart().getY(),
+					((Line) aLine).getEnd().getX(), ((Line) aLine).getEnd().getY());
+			boolean result = line2.intersectsLine(line1);
+			if (!result) {
+
+			}
+
+			else {
+				final double x1, y1, x2, y2, x3, y3, x4, y4;
+
+				x1 = firstPoint.getX();
+				y1 = firstPoint.getY();
+				x2 = secondPoint.getX();
 				y2 = secondPoint.getY();
-                x3 = ((Line) aLine).getStart().getX();                
-				y3 = ((Line) aLine).getStart().getY();                
-				x4 = ((Line) aLine).getEnd().getX();                
+				x3 = ((Line) aLine).getStart().getX();
+				y3 = ((Line) aLine).getStart().getY();
+				x4 = ((Line) aLine).getEnd().getX();
 				y4 = ((Line) aLine).getEnd().getY();
 
-                double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
-                if (denom == 0.0) { // Lines are parallel.
-                }
-                double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3))/denom;
-                double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3))/denom;
-                if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
-                    // Get the intersection point.
-                    intersectedPoint = new Point((x1 + ua*(x2 - x1)),(y1 + ua*(y2 - y1)));
-                    listOfIntersections.add(intersectedPoint);
-                }
-            }
-        }
+				double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+				if (denom == 0.0) { // Lines are parallel.
 
-        if (listOfIntersections.size() == 0) {
-            return null;
-        }
-		
-        return listOfIntersections;
-    }
+				}
+				double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
+				double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+				if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
+					// Get the intersection point.
+					intersectedPoint = new Point((x1 + ua * (x2 - x1)), (y1 + ua * (y2 - y1)));
+					listOfIntersections.add(intersectedPoint);
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((points == null) ? 0 : points.hashCode());
-        
+				}
+			}
+
+		}
+
+		if (listOfIntersections.size() == 0) {
+			return null;
+		}
+		return listOfIntersections;
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((points == null) ? 0 : points.hashCode());
 		return result;
-    }
+	}
 
-    @Override
-    public String toString() {
-        final int maxLen = 10;
-        StringBuilder builder = new StringBuilder();
-        builder.append("Polygon [");
-        if (points != null) {
-            builder.append("points=");
-            builder.append(points.subList(0, Math.min(points.size(), maxLen)));
-        }
-        builder.append("]");
-		
-        return builder.toString();
-    }
+	@Override
+	public String toString() {
+		final int maxLen = 10;
+		StringBuilder builder = new StringBuilder();
+		builder.append("Polygon [");
+		if (points != null) {
+			builder.append("points=");
+			builder.append(points.subList(0, Math.min(points.size(), maxLen)));
+		}
+		builder.append("]");
+		return builder.toString();
+	}
 
-    /**
-     * DO NOT use this method when using polygons. This method is just for lines.
-     */
-    @Override
-    public Point getIntersection(Crossable crossable) {
-        return null;
-    }
+	/**
+	 * DO NOT use this method when using polygons. This method is just for lines.
+	 */
 
-    @Override
-    public List<Point> getIntersectionsPath(Crossable aPath) {
-        return null;
-    }
+	@Override
+	public Point getIntersection(Crossable crossable) {
+		return null;
+	}
+
+	public List<Point> getIntersectionsPath(Crossable aPath) {
+		return null;
+	}
 }
-
--- Peruse/src/test/java/com/peruselabs/peruse/shared/graphics/PathTest.java
+++ Peruse/src/test/java/com/peruselabs/peruse/shared/graphics/PathTest.java
@@ -14,87 +14,88 @@ import org.junit.Test;
 
 /**
  * Unit test for {@link Path}.
- *
+ * 
  * @author Velusamy K. Velu (kool.velu@gmail.com)
  *
  * @since Jan 25, 2018
  *
  */
 public class PathTest {
-    private List<Point> points = new ArrayList<Point>();
-    private Path path;
+	private List<Point> points = new ArrayList<Point>();
+	private Path path;
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        Point point = new Point(1, 1);
-        points.add(point);
-        point = new Point(99, 99);
-        points.add(point);
-        point = new Point(199, 1);
-        points.add(point);
-        point = new Point(200,99);
-        points.add(point);
-        path = new Path(points);
-    }
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		Point point = new Point(1, 1);
+		points.add(point);
+		point = new Point(99, 99);
+		points.add(point);
+		point = new Point(199, 1);
+		points.add(point);
+		point = new Point(200, 99);
+		points.add(point);
+		path = new Path(points);
+	}
 
-    /**
-     * Test method for {@link .Path#getIntersection(.api.Crossable)}.
-     */
-    @Test
-    public void testGetIntersection() {
-        List<Point> intersection = new ArrayList<Point>();
-        List<Point> expected = new ArrayList<Point>();
-        Point point = new Point(50, 50);
-        expected.add(point);
+	/**
+	 * Test method for {@link .Path#getIntersection(.api.Crossable)}.
+	 */
+	@Test
+	public void testGetIntersection() {
+		List<Point> intersection = new ArrayList<Point>();
+		List<Point> expected = new ArrayList<Point>();
+		Point point = new Point(50, 50);
+		expected.add(point);
 
-        Point start = new Point(1, 99);
-        Point end = new Point(99, 1);
-        Line anotherLine = new Line(start, end);
-        intersection = new ArrayList<Point>(path.getIntersectionsPath(anotherLine));    //Normal Case, same case as polygon but only
-        // 1 point of intersection since PATH.
-        assertNotNull(intersection);
-        assertEquals(expected.get(0), intersection.get(0));
+		Point start = new Point(1, 99);
+		Point end = new Point(99, 1);
+		Line anotherLine = new Line(start, end);
+		intersection = new ArrayList<Point>(path.getIntersectionsPath(anotherLine)); // Normal Case, same case as
+																						// polygon but only
+		// 1 point of intersection since PATH.
+		assertNotNull(intersection);
+		assertEquals(expected.get(0), intersection.get(0));
 
-        assertEquals(1, intersection.size());
-        assertTrue(path.isCrossingPath(anotherLine));
-        assertFalse(path.isNotCrossingPath(anotherLine));
+		assertEquals(1, intersection.size());
+		assertTrue(path.isCrossingPath(anotherLine));
+		assertFalse(path.isNotCrossingPath(anotherLine));
 
-        expected.clear();
+		expected.clear();
 
-        start = new Point(0, 50); //Normal case
-        end = new Point(160, 50);
-        Line shortLine = new Line(start, end);
-        expected.add(new Point(50,50));
-        expected.add(new Point(149,50));
-        intersection = new ArrayList<Point>(path.getIntersectionsPath(shortLine));
-        assertEquals(expected.get(0),intersection.get(0));
-        assertEquals(expected.get(1),intersection.get(1));
-        assertEquals(expected.size(),intersection.size());
-        assertNotNull(path.getIntersectionsPath(shortLine));
-        assertTrue(path.isCrossingPath(shortLine));
-        assertFalse(path.isNotCrossingPath(shortLine));
+		start = new Point(0, 50); // Normal case
+		end = new Point(160, 50);
+		Line shortLine = new Line(start, end);
+		expected.add(new Point(50, 50));
+		expected.add(new Point(149, 50));
+		intersection = new ArrayList<Point>(path.getIntersectionsPath(shortLine));
+		assertEquals(expected.get(0), intersection.get(0));
+		assertEquals(expected.get(1), intersection.get(1));
+		assertEquals(expected.size(), intersection.size());
+		assertNotNull(path.getIntersectionsPath(shortLine));
+		assertTrue(path.isCrossingPath(shortLine));
+		assertFalse(path.isNotCrossingPath(shortLine));
 
-        expected.clear();
-        start = new Point(0,50);
-        end = new Point(200 , 50);
-        shortLine = new Line(start, end);
+		expected.clear();
+		start = new Point(0, 50);
+		end = new Point(200, 50);
+		shortLine = new Line(start, end);
 
-        Point intersectedPoint1 = new Point(50,50);
-        Point intersectedPoint2 = new Point(149,50);
-        Point intersectedPoint3 = new Point(199.5,50);
-        expected.add(intersectedPoint1);
-        expected.add(intersectedPoint2);
-        expected.add(intersectedPoint3);
+		Point intersectedPoint1 = new Point(50, 50);
+		Point intersectedPoint2 = new Point(149, 50);
+		Point intersectedPoint3 = new Point(199.5, 50);
+		expected.add(intersectedPoint1);
+		expected.add(intersectedPoint2);
+		expected.add(intersectedPoint3);
 
-        intersection = new ArrayList<Point>(path.getIntersectionsPath(shortLine));   //Same case as polygon, but this has 3 intersections
-        // Since its a path and not a closed polygon!
-        assertEquals(expected.size(),intersection.size());
-        assertEquals(expected.get(0),intersection.get(0));
-        assertEquals(expected.get(1),intersection.get(1));
-        assertEquals(expected.get(2),intersection.get(2));
-    }
-
+		intersection = new ArrayList<Point>(path.getIntersectionsPath(shortLine)); // Same case as polygon, but this has
+																					// 3 intersections
+		// Since its a path and not a closed polygon!
+		assertEquals(expected.size(), intersection.size());
+		assertEquals(expected.get(0), intersection.get(0));
+		assertEquals(expected.get(1), intersection.get(1));
+		assertEquals(expected.get(2), intersection.get(2));
+	}
 }
