ccd8e11e4b0d20ebc5b4737ec09bdc7f8bd107bc
mattAkap <mattakap@gmail.com>
2018-04-21 22:20:27 -0400

    Line got rolled back by accident

--- Peruse/src/main/java/com/peruselabs/peruse/shared/graphics/Line.java
+++ Peruse/src/main/java/com/peruselabs/peruse/shared/graphics/Line.java
@@ -5,6 +5,7 @@
 package com.peruselabs.peruse.shared.graphics;
 
 import com.peruselabs.peruse.shared.graphics.api.Crossable;
+import java.awt.geom.Line2D;
 
 /**
  * It is straight, theoretically without thickness, and may have a starting
@@ -16,119 +17,123 @@ import com.peruselabs.peruse.shared.graphics.api.Crossable;
  *
  */
 public class Line implements Crossable {
-    private Point start;
-    private Point end;
+	private Point start;
+	private Point end;
 
-    public Line() {
-        super();
-    }
+	public Line() {
+		super();
+	}
 
-    public Line(Point start, Point end) {
-        super();
-        this.start = start;
-        this.end = end;
-    }
+	public Line(Point start, Point end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        
-        if (obj == null)
-            return false;
-        
-        if (getClass() != obj.getClass())
-            return false;
-        
-        Line other = (Line) obj;
-        
-        if ((end == null && other.end != null) || !end.equals(other.end))
-                return false;
-        
-        if ((start == null && other.start != null) || !start.equals(other.start))
-            return false;
-            
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Line other = (Line) obj;
+		
+		if ((end == null && other.end != null) || !end.equals(other.end))
+				return false;
+				
+		if ((start == null && other.start != null) || !start.equals(other.start))
+				return false;
+		
+		return true;
+	}
 
-    public Point getEnd() {
-        return end;
-    }
+	public Point getEnd() {
+		return end;
+	}
 
-    @Override
-    public Point getIntersection(Crossable aLine) {
-    	Point ret = new Point();
-    	/*Line2D line1 = new Line2D.Double(start.getX(), start.getY(),end.getX(),end.getY());
-    
-    	Line2D line2 = new Line2D.Double(this.start.getX(),this.start.getY(),this.end.getX(),this.end.getY());
-    	boolean result = line2.intersectsLine(line1);
-    	if(result == false) {
-    		return null;
-    	}else {
-    		 final double x1,y1, x2,y2, x3,y3, x4,y4;
-    		 x1 = start.getX(); y1 = start.getY(); x2 = end.getX(); y2 = end.getY();
-    	        x3 = this.start.getX(); y3 = this.start.getY(); x4 = this.end.getX(); y4 = this.end.getY();
-    	        final double x = (
-    	                (x2 - x1)*(x3*y4 - x4*y3) - (x4 - x3)*(x1*y2 - x2*y1)
-    	                ) /
-    	                (
-    	                (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
-    	                );
-    	        final double y = (
-    	                (y3 - y4)*(x1*y2 - x2*y1) - (y1 - y2)*(x3*y4 - x4*y3)
-    	                ) /
-    	                (
-    	                (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
-    	                );
+	@Override
+	public Point getIntersection(Crossable aLine) {
+		Point ret = new Point();
 
-    	        ret.setX(x);
-    	        ret.setY(y);
-    	        return ret;
+		Line2D line1 = new Line2D.Double(((Line) aLine).start.getX(), ((Line) aLine).start.getY(),
+				((Line) aLine).end.getX(), ((Line) aLine).end.getY());
+		Line2D line2 = new Line2D.Double(this.start.getX(), this.start.getY(), this.end.getX(), this.end.getY());
 
-    	    }*/
-    		 return ret;
-	
-    }
-    
-    
-    public Point getStart() {
-        return start;
-    }
+		boolean result = line2.intersectsLine(line1);
+		if (!result)
+			return null;
+		else {
+			final double x1, y1, x2, y2, x3, y3, x4, y4;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((end == null) ? 0 : end.hashCode());
-        result = prime * result + ((start == null) ? 0 : start.hashCode());
-        return result;
-    }
+			x1 = this.start.getX();
+			y1 = this.start.getY();
+			x2 = this.end.getX();
+			y2 = this.end.getY();
+			x3 = ((Line) aLine).start.getX();
+			y3 = ((Line) aLine).start.getY();
+			x4 = ((Line) aLine).end.getX();
+			y4 = ((Line) aLine).end.getY();
 
-    public void setEnd(Point end) {
-        this.end = end;
-    }
+			double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+			
+			if (denom == 0.0) // Lines are parallel.
+				return null;
+			
+			double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
+			double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+			
+			if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f)
+				return new Point((int) (x1 + ua * (x2 - x1)), (int) (y1 + ua * (y2 - y1))); // Get the intersection point.
 
-    public void setStart(Point start) {
-        this.start = start;
-    }
+			return ret;
+		}
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Line [");
-        if (start != null) {
-            builder.append("start=");
-            builder.append(start);
-            builder.append(", ");
-        }
-        
-        if (end != null) {
-            builder.append("end=");
-            builder.append(end);
-        }
-        
-        builder.append("]");
-        
-        return builder.toString();
-    }
+	public Point getStart() {
+		return start;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((end == null) ? 0 : end.hashCode());
+		result = prime * result + ((start == null) ? 0 : start.hashCode());
+		
+		return result;
+	}
+
+	public void setEnd(Point end) {
+		this.end = end;
+	}
+
+	public void setStart(Point start) {
+		this.start = start;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Line [");
+		
+		if (start != null) {
+			builder.append("start=");
+			builder.append(start);
+			builder.append(", ");
+		}
+		
+		if (end != null) {
+			builder.append("end=");
+			builder.append(end);
+		}
+		
+		builder.append("]");
+		
+		return builder.toString();
+	}
 }
